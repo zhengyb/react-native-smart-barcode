96e11780a1ab2201dfcb9b9069dc6a3d867d3a6b
zhengyb <zhengyb@inhand.com.cn>
2017-10-30 14:43:35 +0800

    fix text prop: declair in RectView.h

--- ios/RCTBarcode/RCTBarcode/RectView.h
+++ ios/RCTBarcode/RCTBarcode/RectView.h
@@ -8,6 +8,6 @@
 @property (nonatomic, copy) NSString *scannerText;
 @property (nonatomic,strong)UILabel *noticeInfoLable;
 
-- (id)initWithScannerRect:(CGRect)scannerRect frame:(CGRect)frame scannerRectCornerColor:(NSString*)scannerRectCornerColor;
+- (id)initWithScannerRect:(CGRect)scannerRect frame:(CGRect)frame scannerRectCornerColor:(NSString*)scannerRectCornerColor scannerText:(NSString*)scannerText;
 
 @end
