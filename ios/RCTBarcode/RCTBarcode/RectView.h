794e8a626899c2d15a10ce424d6dba989c857d7b
zhengyb <zhengyb@inhand.com.cn>
2017-10-30 13:55:27 +0800

    fix noticeInfoLable

--- ios/RCTBarcode/RCTBarcode/RectView.h
+++ ios/RCTBarcode/RCTBarcode/RectView.h
@@ -6,6 +6,7 @@
 @property (nonatomic,assign)CGRect scannerRect;
 @property (nonatomic, copy) NSString *scannerRectCornerColor;
 @property (nonatomic, copy) NSString *scannerText;
+@property (nonatomic,strong)UILabel *noticeInfoLable;
 
 - (id)initWithScannerRect:(CGRect)scannerRect frame:(CGRect)frame scannerRectCornerColor:(NSString*)scannerRectCornerColor;
 
